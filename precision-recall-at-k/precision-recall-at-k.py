117a810c74bcfb25acef89345e1c79fa61dcb0c2
Mehmadullah Sheikh <mehmadullahsheikh@gmail.com>
2026-09-26 20:13:17 +0530

    Solve precision-recall-at-k

---
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -0,0 +1,11 @@
+def precision_recall_at_k(recommended: list, relevant: list, k: int) -> list[float]:
+    """
+    Returns [precision, recall] as a list of two floats.
+    """
+    # Write code here
+    top_k=recommended[:k]
+    relevant_items=set(relevant)
+    hits = sum( item in relevant_items for item in top_k)
+    precision=hits/k
+    recall=hits/len(relevant_items)
+    return [precision,recall]
